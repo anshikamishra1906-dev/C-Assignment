99303b1911246bd53a815ee782f636128f507cd9
anshikamishra1906-dev <anshikamishra1906@gmail.com>
2025-10-21 14:59:14 +0530

    Create Mylibrary.h

---
+++ Mylibrary.h
@@ -0,0 +1,9 @@
+#ifndef ANSHIKA_H
+#define ANSHIKA_H
+
+//Number Functions
+int reverse_digits(int num);
+int is_Armstrong(int num);
+int is_Adams(int num);
+int is_Prime(int num);
+int is_Primepalindrome(int num);
