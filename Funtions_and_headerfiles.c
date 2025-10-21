ca3aa31a67058d8b55e77ccbd143d4d5b34cdfe6
anshikamishra1906-dev <anshikamishra1906@gmail.com>
2025-10-21 17:42:50 +0530

    Update Funtions_and_headerfiles.c

--- Funtions_and_headerfiles.c
+++ Funtions_and_headerfiles.c
@@ -1,1 +1,12 @@
-#include
+#include<stdio.h>
+#include<math.h>
+// reverse digits of a number
+int reverse_digits(int num) {
+  int r=0;
+  while (num>0) {
+      r=r*10+(num%10);
+      num/=10;
+  }
+  return r;
+}
+//check if a number is armstrong or not
