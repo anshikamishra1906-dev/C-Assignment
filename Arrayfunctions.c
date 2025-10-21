dad32ffe7588322400cc07affb88f4f9f11b09c7
anshikamishra1906-dev <anshikamishra1906@gmail.com>
2025-10-21 16:47:30 +0530

    Update Arrayfunctions.c

--- Arrayfunctions.c
+++ Arrayfunctions.c
@@ -61,3 +61,18 @@ int main() {
    //NUMBER FUNCTIONS
    printf("Number functions: /n");
    printf("%d is Armstrong? %s\n", num,is_armstrong(num) ? printf("Yes") : printf("No"));
+   printf("%d is Adams? %s\n", num,is_adams(num) ? printf("Yes") : printf("No"));
+   printf("%d is Prime Palindrome? %s\n", num,is_primepalindrome(num) ? printf("Yes") : printf("No"));
+
+   //ARRAY FUNCTIONS
+   int arry[]= {1,9,2,6,6};
+   int n=5;
+   printf("\nArray functions:\n");
+   display_array(arry,n);
+   reverse_array(arry,n);
+   printf("Max at inddex: %d\n", find_MaxIndex(arry,n));
+   printf("Min at inddex: %d\n", find_MinIndex(arry,n));
+   printf("Average: %.2f\n", find_average(arry,n));
+   printf("Search 4:index %d\n", linearsearch(arry,n,4));
+   return 0;
+}  
