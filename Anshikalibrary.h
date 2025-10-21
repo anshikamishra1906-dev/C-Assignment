ef545aa932c03e67e21f1b0b53de801abbda3433
anshikamishra1906-dev <anshikamishra1906@gmail.com>
2025-10-21 15:28:48 +0530

    Create Anshikalibrary.h

---
+++ Anshikalibrary.h
@@ -0,0 +1,10 @@
+#ifndef ANSHIKA_H
+#DEFINE ANSHIKA_H
+
+void display_array(int arry[], int size);
+int reverse_array(int arry[], int size);
+int find_MaxIndex(int arry[], int size);
+int find_MinIndex(int arry[], int size);
+float find_average(int aary[], int size);
+int linearsearch(int aary[], int size, int value);
+#endif
